c4a25dee748210db9eb798c87f7a10a76e58a97c
Tanya Kashyap <kashyta90@gmail.com>
2025-09-09 21:09:16 -0700

    Updated incorrect path in yaml file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DevSecOps!"
+    return "Hello and Welcome to this demo!"
 
 if __name__ == '__main__':
     app.run(debug=True)
